9cbf9595fb77a13911eafee42aec8797347ada95
PRABU GANESAN K <prabuganesan.kamaraj@chainsys.com>
2021-08-22 12:40:00 +0530

    backup and load process completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # parkingmanagement
+
+Install below softwares
+Nodejs - 10.16.3 (https://nodejs.org/download/release/v10.16.3/)
+Angular - 7.3.5 (npm i -g @angular/cli@7.3.5)
+Ionic - 4.12.0 (npm i - g ionic@4.12.0)
+
+
+After install node module change below things
+Open node_modules/relational-pouch/lib
+Add default in pouch-utils.js (var Promise = require('pouchdb-promise').default;)
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 var PouchDB = require('pouchdb');
+var replicationStream = require('pouchdb-replication-stream');
+PouchDB.adapter('writableStream', replicationStream.adapters.writableStream);
+
+const fs = require('fs');
 var express = require('express');
 var app = express();
 var cors = require('cors')
 app.use(cors())
-
+PouchDB.plugin(replicationStream.plugin);
 app.use('/db', require('express-pouchdb')(PouchDB));
 var parkingPouch = new PouchDB('parking');
 
@@ -11,6 +15,37 @@ app.get('/', (req,res) =>{
     res.send("It works");
 })
 
+
+
+app.get('/backup', (req,res) =>{
+    var ws = fs.createWriteStream('output.txt');
+
+    parkingPouch.dump(ws).then( response=> {
+        console.log(response)
+        fs.close()
+        res.send("Backup completed");
+
+      }).catch(error=>{
+        console.log('err',error)
+        fs.close()
+        res.send("Backup failed");
+      })
+})
+
+app.get('/load', (req,res) =>{
+    var rs = fs.createReadStream('output.txt');
+
+    parkingPouch.load(rs).then( response=> {
+        console.log(response)
+        fs.close()
+        res.send("Loding completed");
+
+      }).catch(error=>{
+        console.log('err',error)
+        fs.close()
+        res.send("Loading failed");
+      })
+})
 //Start server
 app.listen(1000);
 
