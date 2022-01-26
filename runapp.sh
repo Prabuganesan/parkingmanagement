e59d9c988e0bf85ff78c28ec7df874acfee4dd64
Prabu Ganesan <k.prabuganesan@gmail.com>
2022-01-26 15:37:28 +0530

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -2,7 +2,7 @@ echo "Stop server"
 killall node
 echo "Server ready to up"
 node server
-echo "Server running in port number 1000"
+echo "Running at Port 1000. Now you can open http://localhost:1000/"
 sleep 1000
 start http://localhost:1000
 
